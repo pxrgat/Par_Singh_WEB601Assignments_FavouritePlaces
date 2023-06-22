85a37c9dd385340c70da716bcc3aadc60ff8b9d9
pxrgat <W0800281@myscc.ca>
2023-06-22 14:45:11 -0400

    edit

--- Par_Singh_FavouritePlaces/src/app/hover-affect.directive.spec.ts
+++ Par_Singh_FavouritePlaces/src/app/hover-affect.directive.spec.ts
@@ -1,8 +1,38 @@
-import { HoverAffectDirective } from './hover-affect.directive';
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
 
-describe('HoverAffectDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HoverAffectDirective();
-    expect(directive).toBeTruthy();
-  });
-});
+@Directive({
+  selector: '[appHoverAffect]'
+})
+export class HoverAffectDirective {
+  @Input() affectStyle?: string;
+  private originalBorder?: string;
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.addStyles();
+    this.originalBorder = this.elementRef.nativeElement.style.border;
+    this.renderer.setStyle(this.elementRef.nativeElement, 'border', '2px solid blue');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.removeStyles();
+    this.renderer.setStyle(this.elementRef.nativeElement, 'border', this.originalBorder);
+  }
+
+  private addStyles() {
+    if (this.affectStyle === 'underline') {
+      this.renderer.setStyle(this.elementRef.nativeElement, 'text-decoration', 'underline');
+    } else if (this.affectStyle === 'bold') {
+      this.renderer.setStyle(this.elementRef.nativeElement, 'font-weight', 'bold');
+    }
+  }
+
+  private removeStyles() {
+    if (this.affectStyle === 'underline') {
+      this.renderer.removeStyle(this.elementRef.nativeElement, 'text-decoration');
+    } else if (this.affectStyle === 'bold') {
+      this.renderer.removeStyle(this.elementRef.nativeElement, 'font-weight');
+    }
+  }
+}
